92b5c826b9bf230a623d4f31368ba56467b6619d
Mirza Muhammad Aqash <muhammadaqashmirza@gmail.com>
2022-05-25 12:28:40 +0200

    Update Suspension.java

--- examples/src/main/java/com/example/Suspension.java
+++ examples/src/main/java/com/example/Suspension.java
@@ -1,22 +1,80 @@
 package com.example;
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
-public class Suspension
-{
-    //Main Function
-    public static void main(String[] args)
-    {
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
 
+public class Suspension {
+  public static int inventorySize = 10;
+  public static void main(String[] args) {
+    InventoryControl inventoryControl = new InventoryControl();
+    for (int i = 0; i < inventorySize; i++) {
+      inventoryControl.addInventory(i);
     }
+    test(inventoryControl);
+  }
 
-    //FuzzerTestOneInput for randomized inputs
-    public static void fuzzerTestOneInput(FuzzedDataProvider data)
-    {
+  //FuzzerTestOneInput for randomized inputs
+  /*public static void fuzzerTestOneInput(FuzzedDataProvider data)
+  {
 
+  }*/
+
+  //Bug type under test
+  public static void test(InventoryControl inventory) {
+    Thread firstAssociate = new Thread(() -> {
+      synchronized(inventory) {
+        while (!inventory.isEmpty()) {
+          try {
+            inventory.reduceQunatityOnHand();
+            System.out.println("First Associate is getting rid of items");
+            sleep(1000);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+
+    Thread secondAssociate = new Thread(() -> {
+      synchronized(inventory) {
+        while (!inventory.isEmpty()) {
+          try {
+            inventory.reduceQunatityOnHand();
+            System.out.println("Second Associate is getting rid of items");
+            sleep(1000);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+
+    firstAssociate.start();
+    secondAssociate.start();
+  }
+
+  private static void sleep(int delay) {
+    try {
+      Thread.sleep(delay);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    //Bug type under test
-    public static void test()
-    {
-
+  static class InventoryControl {
+    private final Queue < Integer > inventory = new LinkedList < Integer > ();
+    public void addInventory(int itemNumber) {
+      inventory.add(itemNumber);
     }
+    public void reduceQunatityOnHand() throws InterruptedException {
+      inventory.remove();
+    }
+    public boolean isEmpty() {
+      return inventory.isEmpty();
+    }
+    public Integer nextInventory() {
+      return inventory.poll();
+    }
+  }
 }
