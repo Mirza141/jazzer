ebfa1dc3f649f33a74f07edf7c0b00095e351d53
Mirza141 <muhammadaqashmirza@gmail.com>
2022-06-09 13:53:38 +0200

    Increasing N

--- examples/src/main/java/com/example/benchmarks/Database.java
+++ examples/src/main/java/com/example/benchmarks/Database.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
 class Database {
-    final static int N = 128;
+    final static int N = 1280000000;
     private final HashMap<Integer, Entry> entries = new HashMap<Integer, Entry>();
     private int counter = 0;
 
